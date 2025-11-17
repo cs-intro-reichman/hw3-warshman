7bd04fe07a7403270809c36842fad137549e36f1
Apple <Apple@MacBook-Air.local>
2025-11-17 18:31:12 +0200

    fixed some algebra

--- Algebra.java
+++ Algebra.java
@@ -6,49 +6,71 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
+	    // System.out.println(plus(2,3));   // 2 + 3
+		System.out.println(sqrt(5));   // 2 + 3
+	    // System.out.println(minus(7,2));  // 7 - 2
+   		// System.out.println(minus(2,7));  // 2 - 7
+ 		// System.out.println(times(3,4));  // 3 * 4
+   		// System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+   		// System.out.println(pow(5,3));      // 5^3
+   		// System.out.println(pow(3,5));      // 3^5
+   		// System.out.println(div(12,3));   // 12 / 3    
+   		// System.out.println(div(5,5));    // 5 / 5  
+   		// System.out.println(div(25,7));   // 25 / 7
+   		// System.out.println(mod(25,7));   // 25 % 7
+   		// System.out.println(mod(120,6));  // 120 % 6    
+   		// System.out.println(sqrt(36));
+		// System.out.println(sqrt(263169));
+   		// System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-
-		for (int i = 0; i < x2; i++) {
-			x1++;
+		if (x2 < 0) {
+			for (int i = 0; i > x2; i--) {
+				x1--;
+			}	
+		} else {
+			for (int i = 0; i < x2; i++) {
+				x1++;
+			}
 		}
+
 		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		for (int i = 0; i < x2; i++) {
-			x1--;
+		if (x2 < 0) {
+			for (int i = 0; i > x2; i--) {
+				x1++;
+			}
+		} else {
+			for (int i = 0; i < x2; i++) {
+				x1--;
+			}
 		}
+		
 		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		int product = 0;
-		for (int i = 0; i < x1; i++) {
-			for (int j = 0; j < x2; j++) {
+		//gets the positive of x1 and x2
+		int x1pos = (x1 > 0) ? x1 : minus(0, x1);
+		int x2pos = (x2 > 0) ? x2 : minus(0, x2);
+		for (int i = 0; i < x1pos; i++) {
+			for (int j = 0; j < x2pos; j++) {
 				product++;
-				}
+			}
 		}
-		return product;
+
+		if ((x1 < 0 && x2 < 0) | (x1 > 0 && x2 > 0)) {
+			return product;
+		} else {
+			return minus(0, product);
+		}
 	}
 
 
@@ -56,22 +78,43 @@ public class Algebra {
 	public static int pow(int x, int n) {
 		// Replace the following statement with your code
 		int pow = x;
-		for (int i = 1; i < n; i++) {
-			pow = times(pow, x);
+		if (n == 0) {
+			return 1;
+		} else {
+			for (int i = 1; i < n; i++) {
+				pow = times(pow, x);
+			}
 		}
+		
+		
 		return pow;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		//assumes x1 is larger than x2
-		int result = minus(x1, x2);
+		int result = 0;
 		int count = 1;
-		while (result >= x2) {
-			result = minus(result, x2);
-			count++;
+		int x1pos = (x1 > 0) ? x1 : minus(0, x1);
+		int x2pos = (x2 > 0) ? x2 : minus(0, x2);
+
+		if (x2 == 0) {
+			return 1000000;
+		} else if (x1 == 0) {
+			return 0;
+		} else {
+			result = minus(x1pos, x2pos);
+			while (result >= x2pos) {
+						result = minus(result, x2pos);
+						count++;
+					}
 		}
-		return count;
+
+		if ((x1 >= 0 && x2 > 0) | (x1 <= 0 && x2 < 0)) {
+			return count;
+		} else {
+			return minus(0, count);
+		}
 
 	}
 
@@ -90,9 +133,9 @@ public class Algebra {
 		int result = 0;
 	
 		while (result < x) {
-			result = times(power, power);
 			power++;
+			result = times(power, power);
 		}
-		return minus(power, 1);
+		return power;
 	}	  	  
 }
